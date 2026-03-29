fb41a974ac5414b451be56793bf7c288bc917700
ywshiue <151159761+ywshiue@users.noreply.github.com>
2026-03-29 20:11:23 +0800

    Update ceramics.py

--- ceramics.py
+++ ceramics.py
@@ -328,7 +328,7 @@ def glaze_seger_calculator():
                 ha='center', va='bottom',
                 fontsize=5, color='blue', weight='bold')
 
-        ax.text(x, y + size*0.28,
+        ax.text(x, y + size*0.25,
                 "X= " + format_number(row['X_RO (克)']),
                 ha='center', fontsize=4)
 
@@ -336,11 +336,11 @@ def glaze_seger_calculator():
                 "Y= " + format_number(row['Y_RO2 (克)']),
                 ha='center', fontsize=4)
 
-        ax.text(x, y - size*0.28,
+        ax.text(x, y - size*0.25,
                 "Z= " + format_number(row['Z_R2O3 (克)']),
                 ha='center', fontsize=4)
 
-        ax.text(x, y - size*0.48,
+        ax.text(x, y - size*0.5,
                 f"{color_name} {format_number(row['Color (克)'])}",
                 ha='center', fontsize=4, color='green')
 
